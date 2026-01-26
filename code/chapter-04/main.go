e85fda87a22c6056845dd2eade5eedcea70dd468
Tomo <94550718+Tomo1151@users.noreply.github.com>
2026-01-26 11:10:39 +0900

    コードの説明を追加

--- code/chapter-04/main.go
+++ code/chapter-04/main.go
@@ -7,5 +7,9 @@ import (
 func main() {
 	c := cpu.NewCPU()
 
+	// 配列から以下のプログラムを実行
+	// LDA #$24    ; A = $24
+	// AND #$0F    ; A = A & $0F
+	// BRK         ; break0
 	c.RunWithByteArray([]uint8{0xA9, 0x24, 0x29, 0x0F, 0x00})
 }
