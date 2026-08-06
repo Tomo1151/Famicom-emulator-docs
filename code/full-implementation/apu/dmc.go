b725fefec401f61d676374cc2ef0fb5f22ce7e1b
Tomo <94550718+Tomo1151@users.noreply.github.com>
2026-08-06 21:40:57 +0900

    DMCの微修正

--- code/full-implementation/apu/dmc.go
+++ code/full-implementation/apu/dmc.go
@@ -10,42 +10,28 @@ var (
 	}
 )
 
-// MARK: DMCチャンネル
+// MARK: DeltaModulationChannelの定義
 type DeltaModulationChannel struct {
-	register   DMCRegister
-	readMemory MemoryReader
-
-	// Output Unit
+	register      DMCRegister
+	readMemory    MemoryReader
 	shiftRegister DMCShiftRegisteer
-	silence       bool
 
-	// Sample Buffer
-	sampleBuffer      uint8
-	sampleBufferEmpty bool
+	silence           bool   // 無音フラグ
+	sampleBuffer      uint8  // サンプルバッファ
+	sampleBufferEmpty bool   // サンプルバッファの状態
+	timer             uint16 // タイマ
+	timerPeriod       uint16 // チャンネルの周期
+	deltaCounter      uint8  // デルタカウンタ初期値
+	sampleAddress     uint16 // サンプル開始アドレス
+	bytesLeft         uint   // 残りサンプル数
+	irqEnabled        bool   // IRQ有効フラグ
+	loop              bool   // ループフラグ
 
-	// Timer
-	timer       uint16
-	timerPeriod uint16
-
-	// Delta Counter
-	deltaCounter uint8
-
-	// Memory Reader
-	sampleAddress uint16
-	bytesLeft     uint
-
-	// Control
-	irqEnabled bool
-	loop       bool
-
-	// IRQ
-	irq bool
-
-	// Output
-	output float32
+	irq    bool    // 割り込み待機状態
+	output float32 // 出力値
 }
 
-// MARK: コンストラクタ
+// MARK: DeltaModulationChannelのコンストラクタ
 func NewDeltaModulationChannel() *DeltaModulationChannel {
 	return &DeltaModulationChannel{
 		register:          NewDMCRegister(),
@@ -65,38 +51,39 @@ func NewDeltaModulationChannel() *DeltaModulationChannel {
 	}
 }
 
-// MARK: DMCクロック
+// MARK: DMCのクロック
 func (dmc *DeltaModulationChannel) Tick() {
-	// 1. バッファにデータがあり、シフトレジスタが空なら移動
+	// バッファにデータがあり，シフトレジスタが空なら移動
 	dmc.reloadShiftRegister()
 
-	// 2. バッファが空ならメモリから1バイト読み込み
-	dmc.clockMemoryReader()
+	// バッファが空ならメモリから1バイト読み込み
+	dmc.tickMemoryReader()
 
-	// タイマ進行
+	// クロック毎にタイマを進める
 	if dmc.timer > 0 {
 		dmc.timer--
 		return
 	}
 
-	// タイマ再ロード
+	// 分周器の励起時にタイマをリロード
 	if dmc.timerPeriod > 0 {
 		dmc.timer = dmc.timerPeriod - 1
 	}
 
-	// 3. 1bit出力（DAC更新）
-	dmc.clockOutputUnit()
-
+	// 1ビット出力
+	dmc.tickOutputUnit()
 	dmc.output = float32(dmc.deltaCounter)
 }
 
 // MARK: Output Unitのクロック
-func (dmc *DeltaModulationChannel) clockOutputUnit() {
+func (dmc *DeltaModulationChannel) tickOutputUnit() {
+	// シフトレジスタが空のときは何もしない
 	if dmc.shiftRegister.isEmpty() {
 		return
 	}
 
 	if !dmc.silence {
+		// サンプルバッファが空でない場合
 		bit := dmc.shiftRegister.shift()
 
 		if bit != 0 {
@@ -109,66 +96,77 @@ func (dmc *DeltaModulationChannel) clockOutputUnit() {
 			}
 		}
 	} else {
+		// サンプルバッファが空の場合
 		dmc.shiftRegister.shift()
 	}
 
-	// シフトレジスタが空になったら即座に次の充填を試みる
+	// シフトレジスタが空になったら即座にサンプルのフェッチを行う
 	if dmc.shiftRegister.isEmpty() {
 		dmc.reloadShiftRegister()
-		dmc.clockMemoryReader()
+		dmc.tickMemoryReader()
 	}
 }
 
 // MARK: Shift Registerへのロード
 func (dmc *DeltaModulationChannel) reloadShiftRegister() {
+	// シフトレジスタが空でない場合は何もしない
 	if !dmc.shiftRegister.isEmpty() {
 		return
 	}
 
+	// サンプルバッファが空の時は無音フラグをセットする
 	if dmc.sampleBufferEmpty {
 		dmc.silence = true
 		return
 	}
 
-	// Sample Buffer -> Shift Register
+	// サンプルバッファからシフトレジスタにデータをロード
 	dmc.shiftRegister.reset()
 	dmc.shiftRegister.SetValue(dmc.sampleBuffer)
 
+	// サンプルバッファを空にし，無音フラグをクリア
 	dmc.sampleBufferEmpty = true
 	dmc.silence = false
 
-	dmc.clockMemoryReader()
+	// サンプルのフェッチ
+	dmc.tickMemoryReader()
 }
 
 // MARK: Memory Readerのクロック
-func (dmc *DeltaModulationChannel) clockMemoryReader() {
+func (dmc *DeltaModulationChannel) tickMemoryReader() {
+	// サンプルバッファ，未再生のサンプルが残っている場合は何もしない
 	if !dmc.sampleBufferEmpty || dmc.bytesLeft == 0 {
 		return
 	}
 
+	// サンプルアドレスから1バイト分フェッチして空フラグをクリア
 	if dmc.readMemory != nil {
 		dmc.sampleBuffer = dmc.readMemory(dmc.sampleAddress)
 		dmc.sampleBufferEmpty = false
 	}
 
+	// フェッチ後にサンプルアドレスをインクリメントし，残りサンプル数をデクリメント
 	dmc.sampleAddress++
 	if dmc.sampleAddress == 0 {
-		dmc.sampleAddress = 0x8000
+		dmc.sampleAddress = 0x8000 // オーバーフロー時には最初に戻す
 	}
-
 	dmc.bytesLeft--
 
+	// 残りサンプルが無くなったとき
 	if dmc.bytesLeft == 0 {
 		if dmc.loop {
+			// ループフラグが有効であればリロード
 			dmc.reload()
 		} else if dmc.irqEnabled {
+			// IRQが有効の場合はIRQを発生させる
 			dmc.irq = true
 		}
 	}
 }
 
-// MARK: DMC再ロード
+// MARK: DMCのリロード
 func (dmc *DeltaModulationChannel) reload() {
+	// サンプルアドレスとサンプル長をレジスタからロード
 	dmc.sampleAddress = 0xC000 + (uint16(dmc.register.sampleAddress) << 6)
 	dmc.bytesLeft = (uint(dmc.register.sampleLength) << 4) + 1
 }
@@ -176,15 +174,19 @@ func (dmc *DeltaModulationChannel) reload() {
 // MARK: DMC有効/無効 ($4015書き込み時)
 func (dmc *DeltaModulationChannel) SetEnabled(enabled bool) {
 	if enabled {
+		// 有効にするとき，再生が終わっていたらリロードとフェッチを行う
 		if dmc.bytesLeft == 0 {
 			dmc.reload()
-			dmc.clockMemoryReader()
+			dmc.tickMemoryReader()
 		}
 	} else {
+		// 無効にするとき，再生を終えて全てをクリア
 		dmc.bytesLeft = 0
 		dmc.sampleBufferEmpty = true
 		dmc.shiftRegister.reset()
 	}
+
+	// ステータスレジスタ書き込みでIRQはクリアされる
 	dmc.irq = false
 }
 
@@ -194,46 +196,66 @@ func (dmc *DeltaModulationChannel) write(address uint16, value uint8) {
 
 	switch address {
 	case 0x4010:
+		/*
+			$4010 書き込み
+			- IRQフラグ
+			- ループフラグ
+			- 再生レート
+		*/
 		dmc.irqEnabled = (value & 0x80) != 0
 		dmc.loop = (value & 0x40) != 0
 		dmc.timerPeriod = DMC_PITCH_TABLE[value&0x0F]
 
+		// IRQ無効化時には待機中のIRQもクリアされる
 		if !dmc.irqEnabled {
 			dmc.irq = false
 		}
-
 	case 0x4011:
+		/*
+			$4011 書き込み
+			- デルタカウンタ初期値
+		*/
 		dmc.deltaCounter = value & 0x7F
-
 	case 0x4012:
-		// Sample Address (register内部保持)
+		/*
+			$4012 書き込み
+			- サンプルアドレス
 
+			※ 初期値をレジスタのみにロード
+			　 リロードのタイミングでセットされる
+		*/
 	case 0x4013:
-		// Sample Length (register内部保持)
+		/*
+			$4013 書き込み
+			- サンプル長
+
+			※ 初期値をレジスタのみにロード
+			　 リロードのタイミングでセットされる
+		*/
 	}
 }
 
-// MARK: DMC出力
+// MARK: DMC出力の取得
 func (dmc *DeltaModulationChannel) Output() float32 {
 	return dmc.output
 }
 
-// MARK: IRQ取得
+// MARK: IRQの取得
 func (dmc *DeltaModulationChannel) IRQ() bool {
 	return dmc.irq
 }
 
-// MARK: IRQ設定
+// MARK: IRQのセット
 func (dmc *DeltaModulationChannel) SetIRQ(value bool) {
 	dmc.irq = value
 }
 
-// MARK: 再生中か ($4015 Bit 4 読出用)
+// MARK: 再生中フラグの取得
 func (dmc *DeltaModulationChannel) IsActive() bool {
 	return dmc.bytesLeft > 0 || !dmc.sampleBufferEmpty
 }
 
-// MARK: Memory Reader設定
+// MARK: Memory Readerのセット
 func (dmc *DeltaModulationChannel) SetMemoryReader(reader MemoryReader) {
 	dmc.readMemory = reader
 }
